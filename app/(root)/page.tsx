4b8c58ed8cdc726f946037a24ca893f56f236939
alexisdlr <alexisdlr3009@hotmail.com>
2023-07-05 13:07:05 -0600

    added authentication with clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return <div>hola</div>;
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
