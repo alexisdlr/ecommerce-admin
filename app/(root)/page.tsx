91a844a7dab65339f6aba9b5938ee5a59477a438
alexisdlr <alexisdlr3009@hotmail.com>
2023-07-06 23:07:04 -0600

    added form create store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
-
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { useEffect } from "react";
 export default function SetupPage() {
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [onOpen, isOpen]);
+
+  return <div className="p-4">root page</div>;
 }
