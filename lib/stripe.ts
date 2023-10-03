7ff2d27551f0b90030f269e2e69f615776033c9e
alexisdlr <alexisdlr3009@hotmail.com>
2023-10-03 16:24:41 -0600

    stripe integration

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_APIKE!, {
+export const stripe = new Stripe(process.env.STRIPE_APIKEY!, {
   apiVersion: "2023-08-16",
   typescript: true
 } )
